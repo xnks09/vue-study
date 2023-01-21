116109fa033140f7c34e367a679bba4153aff4a0
nks <xnks@gsitm.com>
2023-01-21 21:10:16 +0900

    [vue-todo] 1차 수정본

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -14,6 +14,33 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  created(){
+    if(localStorage.length > 0){
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  props: ['propsdata'],
+  methods: {
+    addTodo(todoItem){
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
   components: {
     'TodoHeader': TodoHeader,
     'TodoInput': TodoInput,
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,13 +1,67 @@
 <template>
-    <div>list</div>
-  </template>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in propsdata" class="shadow">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem,index)">
+            <i class="fa fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>    
+</template>
   
-  <script>
-  export default {
-  
+<script>
+export default {
+  props: ['propsdata'],
+  // data(){
+  //   return {
+  //     todoItems: []
+  //   }
+  // },
+  // created(){
+  //   if(localStorage.length > 0){
+  //     for (let i = 0; i < localStorage.length; i++) {
+  //       this.todoItems.push(localStorage.key(i));
+  //     }
+  //   }
+  // },
+  methods: {
+    removeTodo(todoItem,index) {
+      // localStorage.removeItem(todoItem);
+      // this.todoItems.splice(index, 1);
+      this.$emit('removeTodo', todoItem, index);
+    }
   }
-  </script>
-  
-  <style>
-  
-  </style>
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+</style>
